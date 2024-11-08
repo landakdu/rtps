fd5d3bec2f78c222b7bc5b74b371a69b4b46f89c
landakdu <durilandak429@gmail.com>
2024-11-08 15:36:24 +0800

    Update content.js

--- content.js
+++ content.js
@@ -4,34 +4,34 @@ const navbar1 = `
 <div class="mobile-container">
 <!-- Top Navigation Menu -->
 <div class="topnav">
-  <a href="./index.html" class="active tmenu kekanan01" style="background-image: url(./game/icon/home.png); animation-delay:0.5s;"></a>
+  <a href="./index.html" class="active tmenu kekanan01" style="background-image: url(./game/icon/home.png);"></a>
   <div id="myLinks">
-    <a href="./game/pragmatic/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/pragmatic.png)"></a>
-    <a href="./game/pgsoft/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/pgsoft.png)"></a>
-    <a href="./game/spadegaming/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/spadegaming.png)"></a>
-    <a href="./game/fastspin/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/fastspin.png)"></a>
-    <a href="./game/joker/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/joker.png)"></a>
-    <a href="./game/boongo/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/boongo.png)"></a>
-    <a href="./game/jili/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/jili.png)"></a>
-    <a href="./game/playtech/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/playtech.png)"></a>
-    <a href="./game/microgaming/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/microgaming.png)"></a>
-    <a href="./game/habanero/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/habanero.png)"></a>
-    <a href="./game/cq9/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/cq9.png)"></a>
-    <a href="./game/playstar/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/playstar.png)"></a>
-    <a href="./game/yggdrasil/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/yggdrasil.png)"></a>
-    <a href="./game/netent/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/netent.png)"></a>
-    <a href="./game/skywind/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/skywind.png)"></a>
-    <a href="./game/toptrendgaming/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/toptrendgaming.png)"></a>
-    <a href="./game/nextspin/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/nextspin.png)"></a>
-    <a href="./game/bbin/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/bbin.png)"></a>
-    <a href="./game/creativegaming/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/creativegaming.png)"></a>
-    <a href="./game/evoplay/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/evoplay.png)"></a>
-    <a href="./game/royalslotgaming/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/royalgaming.png)"></a>
-    <a href="./game/fishing/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/fishing.png)"></a>
-    <a href="./game/arcade/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/arcade.png)"></a>
-    <a href="./game/casino/index.html" class="stay tmenu gmenu" style="background-image: url(./game/icon/casino.png)"></a>
+    <a href="./game/pragmatic/index.html" class="stay tmenu gmenu kebawah2" style="background-image: url(./game/icon/pragmatic.png)"></a>
+    <a href="./game/pgsoft/index.html" class="stay tmenu gmenu kebawah3" style="background-image: url(./game/icon/pgsoft.png)"></a>
+    <a href="./game/spadegaming/index.html" class="stay tmenu gmenu kebawah04" style="background-image: url(./game/icon/spadegaming.png)"></a>
+    <a href="./game/fastspin/index.html" class="stay tmenu gmenu kebawah5" style="background-image: url(./game/icon/fastspin.png)"></a>
+    <a href="./game/joker/index.html" class="stay tmenu gmenu kebawah6" style="background-image: url(./game/icon/joker.png)"></a>
+    <a href="./game/boongo/index.html" class="stay tmenu gmenu kebawah7" style="background-image: url(./game/icon/boongo.png)"></a>
+    <a href="./game/jili/index.html" class="stay tmenu gmenu kebawah8" style="background-image: url(./game/icon/jili.png)"></a>
+    <a href="./game/playtech/index.html" class="stay tmenu gmenu kebawah9" style="background-image: url(./game/icon/playtech.png)"></a>
+    <a href="./game/microgaming/index.html" class="stay tmenu gmenu kebawah10" style="background-image: url(./game/icon/microgaming.png)"></a>
+    <a href="./game/habanero/index.html" class="stay tmenu gmenu kebawah11" style="background-image: url(./game/icon/habanero.png)"></a>
+    <a href="./game/cq9/index.html" class="stay tmenu gmenu kebawah12" style="background-image: url(./game/icon/cq9.png)"></a>
+    <a href="./game/playstar/index.html" class="stay tmenu gmenu kebawah13" style="background-image: url(./game/icon/playstar.png)"></a>
+    <a href="./game/yggdrasil/index.html" class="stay tmenu gmenu kebawah14" style="background-image: url(./game/icon/yggdrasil.png)"></a>
+    <a href="./game/netent/index.html" class="stay tmenu gmenu kebawah15" style="background-image: url(./game/icon/netent.png)"></a>
+    <a href="./game/skywind/index.html" class="stay tmenu gmenu kebawah16" style="background-image: url(./game/icon/skywind.png)"></a>
+    <a href="./game/toptrendgaming/index.html" class="stay tmenu gmenu kebawah17" style="background-image: url(./game/icon/toptrendgaming.png)"></a>
+    <a href="./game/nextspin/index.html" class="stay tmenu gmenu kebawah18" style="background-image: url(./game/icon/nextspin.png)"></a>
+    <a href="./game/bbin/index.html" class="stay tmenu gmenu kebawah19" style="background-image: url(./game/icon/bbin.png)"></a>
+    <a href="./game/creativegaming/index.html" class="stay tmenu gmenu kebawah20" style="background-image: url(./game/icon/creativegaming.png)"></a>
+    <a href="./game/evoplay/index.html" class="stay tmenu gmenu kebawah21" style="background-image: url(./game/icon/evoplay.png)"></a>
+    <a href="./game/royalslotgaming/index.html" class="stay tmenu gmenu kebawah22" style="background-image: url(./game/icon/royalgaming.png)"></a>
+    <a href="./game/fishing/index.html" class="stay tmenu gmenu kebawah23" style="background-image: url(./game/icon/fishing.png)"></a>
+    <a href="./game/arcade/index.html" class="stay tmenu gmenu kebawah24" style="background-image: url(./game/icon/arcade.png)"></a>
+    <a href="./game/casino/index.html" class="stay tmenu gmenu kebawah25" style="background-image: url(./game/icon/casino.png)"></a>
   </div>
-  <a href="javascript:void(0);" class="icon tmenu kekiri01" onclick="myFunction()" style="background-image: url(./game/icon/game.png); animation-delay:0.15s;"></a>
+  <a href="javascript:void(0);" class="icon tmenu kekiri01" onclick="myFunction()" style="background-image: url(./game/icon/game.png);"></a>
 </div>
 `;
 
@@ -40,43 +40,43 @@ const navbar2 = `
 <div class="mobile-container">
 <!-- Top Navigation Menu -->
 <div class="topnav">
-  <a href="../../index.html" class="active tmenu kekanan01" style="background-image: url(../icon/home.png); animation-delay:0.5s;"></a>
+  <a href="../../index.html" class="active tmenu kekanan01" style="background-image: url(../icon/home.png);"></a>
   <div id="myLinks">
-    <a href="../pragmatic/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/pragmatic.png)"></a>
-    <a href="../pgsoft/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/pgsoft.png)"></a>
-    <a href="../spadegaming/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/spadegaming.png)"></a>
-    <a href="../fastspin/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/fastspin.png)"></a>
-    <a href="../joker/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/joker.png)"></a>
-    <a href="../boongo/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/boongo.png)"></a>
-    <a href="../jili/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/jili.png)"></a>
-    <a href="../playtech/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/playtech.png)"></a>
-    <a href="../microgaming/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/microgaming.png)"></a>
-    <a href="../habanero/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/habanero.png)"></a>
-    <a href="../cq9/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/cq9.png)"></a>
-    <a href="../playstar/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/playstar.png)"></a>
-    <a href="../yggdrasil/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/yggdrasil.png)"></a>
-    <a href="../netent/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/netent.png)"></a>
-    <a href="../skywind/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/skywind.png)"></a>
-    <a href="../toptrendgaming/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/toptrendgaming.png)"></a>
-    <a href="../nextspin/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/nextspin.png)"></a>
-    <a href="../bbin/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/bbin.png)"></a>
-    <a href="../creativegaming/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/creativegaming.png)"></a>
-    <a href="../evoplay/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/evoplay.png)"></a>
-    <a href="../royalslotgaming/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/royalgaming.png)"></a>
-    <a href="../fishing/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/fishing.png)"></a>
-    <a href="../arcade/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/arcade.png)"></a>
-    <a href="../casino/index.html" class="stay tmenu gmenu" style="background-image: url(../icon/casino.png)"></a>
+    <a href="../pragmatic/index.html" class="stay tmenu gmenu kebawah2" style="background-image: url(../icon/pragmatic.png)"></a>
+    <a href="../pgsoft/index.html" class="stay tmenu gmenu kebawah3" style="background-image: url(../icon/pgsoft.png)"></a>
+    <a href="../spadegaming/index.html" class="stay tmenu gmenu kebawah4" style="background-image: url(../icon/spadegaming.png)"></a>
+    <a href="../fastspin/index.html" class="stay tmenu gmenu kebawah5" style="background-image: url(../icon/fastspin.png)"></a>
+    <a href="../joker/index.html" class="stay tmenu gmenu kebawah6" style="background-image: url(../icon/joker.png)"></a>
+    <a href="../boongo/index.html" class="stay tmenu gmenu kebawah7" style="background-image: url(../icon/boongo.png)"></a>
+    <a href="../jili/index.html" class="stay tmenu gmenu kebawah8" style="background-image: url(../icon/jili.png)"></a>
+    <a href="../playtech/index.html" class="stay tmenu gmenu kebawah9" style="background-image: url(../icon/playtech.png)"></a>
+    <a href="../microgaming/index.html" class="stay tmenu gmenu kebawah10" style="background-image: url(../icon/microgaming.png)"></a>
+    <a href="../habanero/index.html" class="stay tmenu gmenu kebawah1" style="background-image: url(../icon/habanero.png)"></a>
+    <a href="../cq9/index.html" class="stay tmenu gmenu kebawah12" style="background-image: url(../icon/cq9.png)"></a>
+    <a href="../playstar/index.html" class="stay tmenu gmenu kebawah13" style="background-image: url(../icon/playstar.png)"></a>
+    <a href="../yggdrasil/index.html" class="stay tmenu gmenu kebawah14" style="background-image: url(../icon/yggdrasil.png)"></a>
+    <a href="../netent/index.html" class="stay tmenu gmenu kebawah15" style="background-image: url(../icon/netent.png)"></a>
+    <a href="../skywind/index.html" class="stay tmenu gmenu kebawah16" style="background-image: url(../icon/skywind.png)"></a>
+    <a href="../toptrendgaming/index.html" class="stay tmenu gmenu kebawah17" style="background-image: url(../icon/toptrendgaming.png)"></a>
+    <a href="../nextspin/index.html" class="stay tmenu gmenu kebawah18" style="background-image: url(../icon/nextspin.png)"></a>
+    <a href="../bbin/index.html" class="stay tmenu gmenu kebawah19" style="background-image: url(../icon/bbin.png)"></a>
+    <a href="../creativegaming/index.html" class="stay tmenu gmenu kebawah20" style="background-image: url(../icon/creativegaming.png)"></a>
+    <a href="../evoplay/index.html" class="stay tmenu gmenu kebawah21" style="background-image: url(../icon/evoplay.png)"></a>
+    <a href="../royalslotgaming/index.html" class="stay tmenu gmenu kebawah22" style="background-image: url(../icon/royalgaming.png)"></a>
+    <a href="../fishing/index.html" class="stay tmenu gmenu kebawah23" style="background-image: url(../icon/fishing.png)"></a>
+    <a href="../arcade/index.html" class="stay tmenu gmenu kebawah24" style="background-image: url(../icon/arcade.png)"></a>
+    <a href="../casino/index.html" class="stay tmenu gmenu kebawah25" style="background-image: url(../icon/casino.png)"></a>
   </div>
-  <a href="javascript:void(0);" class="icon tmenu kekiri01" onclick="myFunction()" style="background-image: url(../icon/game.png); animation-delay:0.15s;"></a>
+  <a href="javascript:void(0);" class="icon tmenu kekiri01" onclick="myFunction()" style="background-image: url(../icon/game.png);"></a>
 </div>
 `;
 
 // SEARCH
 const search1 = `
-<div class="keatas01" style="animation-delay:0s"><input id="searchbar" onkeyup="search_animal()" type="text" name="search" placeholder="cari nama permainan..."></div>
+<div class="keatas01"><input id="searchbar" onkeyup="search_animal()" type="text" name="search" placeholder="cari nama permainan..."></div>
 `;
 
 // MENU UTAMA
 const menuutama1 = `
-<div class="posisimenuutama"><div class="menuutama" style="background-image: url(./game/icon/game.png); animation-delay:0.0s; href="javascript:void(0); onclick="myFunction()"></div></div>
+<div class="posisimenuutama"><div class="menuutama kebawah1" style="background-image: url(./game/icon/game.png); href="javascript:void(0); onclick="myFunction()"></div></div>
 `;
